e15b60aeb1c3508367c6f0d5ea3234d6c4846692
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-08-31 12:19:11 -0700

    refractored code

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 
-class Navbar extends Component {
-    static defaultProps = {
-        title: 'Github Finder',
-        icon: 'fab fa-github'
-    };
+const Navbar = (props) => {
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    };
-
-    render() {
-        return (
-            <nav className='navbar bg-primary'>
-                <h1>
-                    <i className={this.props.icon} /> {this.props.title}
-                </h1>
-                <ul>
+    return (
+        <nav className='navbar bg-primary'>
+            <h1>
+                <i className={props.icon} /> {props.title}
+            </h1>
+            {/* <ul>
                     <li><a get='#'>Home</a></li>
                     <li><a get='#'>About</a></li>
-                </ul>
-            </nav>
-        )
-    }
-}
+                </ul> */}
+        </nav>
+    );
+};
+
+
+//proptypes & default props
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
+
+
 
 export default Navbar
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-class UserItem extends Component {
-    state = {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-    }
+//can be refractor to instead of (props) -> ({user: {login, avatar_url, html_url}})
+const UserItem = (props) => {
+    const { login, avatar_url, html_url } = props.user;
+    return (
 
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
-        return (
-
-            <div className='card text-center'>
-                <img src={avatar_url} alt="avatar" className='round-img' style={{ width: '60px' }} />
-                <h3>{login}</h3>
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
+        <div className='card text-center'>
+            <img src={avatar_url} alt="avatar" className='round-img' style={{ width: '60px' }} />
+            <h3>{login}</h3>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 export default UserItem
