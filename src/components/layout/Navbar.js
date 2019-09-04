05a114fa591491bc7a211994b3f3f94c65e25746
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-09-04 13:23:42 -0700

    React router for pages

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
 
@@ -8,10 +9,10 @@ const Navbar = (props) => {
             <h1>
                 <i className={props.icon} /> {props.title}
             </h1>
-            {/* <ul>
-                    <li><a get='#'>Home</a></li>
-                    <li><a get='#'>About</a></li>
-                </ul> */}
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+            </ul>
         </nav>
     );
 };
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
     // state for text value 
