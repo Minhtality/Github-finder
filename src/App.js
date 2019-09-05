e8aa41051a61eb8ad01ab745ef1cb51e8f6d1c00
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-09-04 21:07:36 -0700

    sort repo by desc

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
   // repo api request
   getRepos = async (username) => {
     this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=6&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=6&sort=pushed:desc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ repos: res.data, loading: false });
   }
   // api search state
